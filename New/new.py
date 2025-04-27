17545f854cb9dbda1da7e44a1aeefb8d059e178b
Leonard <Oseghaleleonard39@gmail.com>
2025-04-27 00:55:42 -0700

    Initial commit

---
+++ New/new.py
@@ -0,0 +1,11 @@
+# Frist python code 
+
+# print("Hello world!")
+
+first_name = "Leonard"
+food = "Hamburger"
+email = "oseghaleleonard39@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"I like {food}")
+print(f"Send the document to {email}")
